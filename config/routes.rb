d77192db5d3cf022b69cd0a5c5b895e66419badf
juan mendoza <j.mendoza712@gmail.com>
2022-11-16 09:05:33 +0000

    refactoring needed

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,17 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # Landing page
+  get "tasks", to: "tasks#index"
+  # CREATE
+  get "tasks/new", to: "tasks#new"
+  post "tasks", to: "tasks#create"
+  # Displaying each task
+  get "tasks/:id", to: "tasks#show", as: :task
+  # UPDATE
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  patch "tasks/:id", to: "tasks#update"
+  # DELETE
+  delete "tasks/:id", to: "tasks#destroy", as: :delete_task
 end
